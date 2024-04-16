d606f3d63baf85771b553c6c046937f769b9165c
EMR <emdev144@gmail.com>
2024-04-16 18:30:33 -0400

    infinite scroll apiURL quotes `` updates

--- script.js
+++ script.js
@@ -1,25 +1,16 @@
 // Unsplash API
-
 const count = 10;
-const apiKey = 'SpHFO3HwaT9Kngog-TE2MWc8Vz0pXC6uffOdfoZ_3u0'
-console.log(apiKey);
-const apiUrl = 'https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}';
-
-console.log(apiUrl);
-
+const apiKey = 'SpHFO3HwaT9Kngog-TE2MWc8Vz0pXC6uffOdfoZ_3u0';
+const apiUrl =`https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
 
 // Get photos from Unsplash API
-
 async function getPhotos() {
-
     try {
-
         const response = await fetch(apiUrl);
         const data = await response.json();
         console.log(data);
-
     } catch (error) {
-        // Catch error here
+        // Catch Error Here
     }
 }
 
